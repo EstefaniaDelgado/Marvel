85662870c6fe20c261bd3d51292f079b8d6295aa
EstefaniaDelgado <teff_64@hotmail.com>
2025-09-16 21:34:58 -0500

    style(characters): add styles accoding to design

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,2 +1,23 @@
-// API services and external integrations will be exported from here
-export {};
+import CryptoJS from "crypto-js";
+
+export const MARVEL_CONFIG = {
+  publicKey: import.meta.env.VITE_MARVEL_PUBLIC_KEY,
+  privateKey: import.meta.env.VITE_MARVEL_PRIVATE_KEY,
+  baseUrl: "https://gateway.marvel.com/v1/public",
+};
+
+export const generateMD5 = (keys: string) => {
+  return CryptoJS.MD5(keys).toString();
+};
+
+
+export const generateMarvelAuth = () => {
+  const timestamp = Date.now().toString();
+  const hash = generateMD5(timestamp + MARVEL_CONFIG.privateKey + MARVEL_CONFIG.publicKey);
+
+  return {
+    ts: timestamp,
+    apikey: MARVEL_CONFIG.publicKey,
+    hash: hash,
+  };
+};
